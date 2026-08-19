8c4f779d76c69edb37d62912e39104130d8fb4b9
sophia-w-hash <sophia@wwkart.com>
2026-08-20 04:29:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.post('/api/send-direct', async (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         pool: true,
-        maxConnections: 3,
+        maxConnections: 1,
         maxMessages: 100,
         rateDelta: 1000,
         auth: {
